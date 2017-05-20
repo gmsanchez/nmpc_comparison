6e3d52d5294f353428ec21378fc8bb331239a197
Guido Sanchez <sanchez.guido@gmail.com>
2017-05-20 14:23:41 -0300

    Add LTV discretization and modify stats output.

--- comparison_casadi_ltv.py
+++ comparison_casadi_ltv.py
@@ -5,15 +5,56 @@ import casadi.tools as ctools
 import numpy as np
 import matplotlib.pyplot as plt
 
+import scipy.linalg
 import time
+
 #<<ENDCHUNK>>
+
+def c2d(A, B, Delta, Bp=None, f=None, asdict=False):
+    """
+    Discretizes affine system (A, B, Bp, f) with timestep Delta.
+    This includes disturbances and a potentially nonzero steady-state, although
+    Bp and f can be omitted if they are not present.
+    If asdict=True, return value will be a dictionary with entries A, B, Bp,
+    and f. Otherwise, the return value will be a 4-element list [A, B, Bp, f]
+    if Bp and f are provided, otherwise a 2-element list [A, B].
+    """
+    n = A.shape[0]
+    I = np.eye(n)
+    D = scipy.linalg.expm(Delta*np.vstack((np.hstack([A, I]),
+                                           np.zeros((n, 2*n)))))
+    Ad = D[:n, :n]
+    Id = D[:n, n:]
+    Bd = Id.dot(B)
+    Bpd = None if Bp is None else Id.dot(Bp)
+    fd = None if f is None else Id.dot(f)
+
+    if asdict:
+        retval = dict(A=Ad, B=Bd, Bp=Bpd, f=fd)
+    elif Bp is None and f is None:
+        retval = [Ad, Bd]
+    else:
+        retval = [Ad, Bd, Bpd, fd]
+    return retval
+    
+def _calc_lin_disc_wrapper_for_mp_map(item):
+    """ Function wrapper for map or multiprocessing.map . """
+    _fi, _xi, _ui, _Delta = item
+    Ai = _fi.jacobian(0, 0)(_xi, _ui)[0].full()
+    Bi = _fi.jacobian(1, 0)(_xi, _ui)[0].full()
+    # Gi = _fi.jacobian(2, 0)(_xi, _ui)[0].full()
+    Ei = _fi(_xi, _ui).full().ravel() - Ai.dot(_xi).ravel() - Bi.dot(_ui).ravel() # - Gi.dot(_wi).ravel()
+    # [Ai[:], Bi[:], Gi[:], Ei[:]] = c2d(Ai, Bi, _Delta, Gi, Ei)
+    [Ai[:], Bi[:], _, Ei[:]] = c2d(Ai, Bi, _Delta, f=Ei)
+    return Ai, Bi, Ei
+    
 
 # Define model and get simulator.
 Delta = .25
-Nt = 20
+Nt = 10
 Nx = 2
 Nu = 1
-def ode(x,u):
+def ode(x,u,w=0):
     dxdt = [
         (1 - x[1]*x[1])*x[0] - x[1] + u,
         x[0]]
@@ -43,13 +84,13 @@ vdp = casadi.integrator("int_ode",
 ode_casadi = casadi.Function(
     "ode",[x,u],[ode(x,u)])
 
-k1 = ode_casadi(x, u)
-k2 = ode_casadi(x + Delta/2*k1, u)
-k3 = ode_casadi(x + Delta/2*k2, u)
-k4 = ode_casadi(x + Delta*k3,u)
-xrk4 = x + Delta/6*(k1 + 2*k2 + 2*k3 + k4)    
-ode_rk4_casadi = casadi.Function(
-    "ode_rk4", [x,u], [xrk4])
+#k1 = ode_casadi(x, u)
+#k2 = ode_casadi(x + Delta/2*k1, u)
+#k3 = ode_casadi(x + Delta/2*k2, u)
+#k4 = ode_casadi(x + Delta*k3,u)
+#xrk4 = x + Delta/6*(k1 + 2*k2 + 2*k3 + k4)    
+#ode_rk4_casadi = casadi.Function(
+#    "ode_rk4", [x,u], [xrk4])
 
 #<<ENDCHUNK>>
 
@@ -71,7 +112,7 @@ ulb = -.75
 
 # Make optimizers.
 x0 = np.array([0,1])
-
+u0 = np.array([0])
 # Create variables struct.
 var = ctools.struct_symSX([(
     ctools.entry("x",shape=(Nx,),repeat=Nt+1),
@@ -81,6 +122,19 @@ varlb = var(-np.inf)
 varub = var(np.inf)
 varguess = var(0)
 
+# Create parameters struct.
+par = ctools.struct_symSX([
+    ctools.entry("Ad", repeat=Nt, shape=(Nx, Nx)),
+    ctools.entry("Bd", repeat=Nt, shape=(Nx, Nu)),
+    ctools.entry("fd", repeat=Nt, shape=(Nx, 1))])
+parguess = par(0)
+
+# Set initial values to parameters
+[A0, B0, f0] = _calc_lin_disc_wrapper_for_mp_map([ode_casadi,x0,u0,Delta])
+for t in range(Nt):
+    parguess['Ad',t] = A0
+    parguess['Bd',t] = B0
+    parguess['fd',t] = f0
 # Adjust the relevant constraints.
 for t in range(Nt):
     varlb["u",t,:] = ulb
@@ -90,8 +144,10 @@ for t in range(Nt):
 obj = casadi.SX(0)
 con = []
 for t in range(Nt):
-    con.append(ode_rk4_casadi(var["x",t],
-        var["u",t]) - var["x",t+1])
+    con.append(var["x", t+1] -
+        casadi.mtimes(par["Ad", t], var["x", t]) -
+        casadi.mtimes(par["Bd", t], var["u", t]) -
+        par["fd",t])                                 
     obj += l(var["x",t], var["u",t])
 obj += Pf(var["x",Nt])
 
@@ -100,11 +156,14 @@ con = casadi.vertcat(*con)
 conlb = np.zeros((Nx*Nt,))
 conub = np.zeros((Nx*Nt,))
 
-nlp = dict(x=var, f=obj, g=con)
+nlp = dict(x=var, f=obj, g=con, p=par)
 nlpoptions = {
     "ipopt" : {
         "print_level" : 0,
         "max_cpu_time" : 60,
+        "jac_c_constant" : "yes",
+        "jac_d_constant" : "yes",
+        "hessian_constant" : "yes"
     },
     "print_time" : False,
 }
@@ -112,6 +171,7 @@ solver = casadi.nlpsol("solver",
     "ipopt", nlp, nlpoptions)
 
 #<<ENDCHUNK>>
+
 
 # Now simulate.
 Nsim = 40
@@ -121,7 +181,7 @@ x[0,:] = x0
 u = np.zeros((Nsim,Nu))
 for t in range(Nsim):
     t0 = time.time()
-    # Fix initial state.    
+    # Fix initial state.
     varlb["x",0,:] = x[t,:]
     varub["x",0,:] = x[t,:]
     varguess["x",0,:] = x[t,:]
@@ -129,7 +189,8 @@ for t in range(Nsim):
                 lbx=varlb,
                 ubx=varub,
                 lbg=conlb,
-                ubg=conub)   
+                ubg=conub,
+                p=parguess)   
     
     #<<ENDCHUNK>>    
     
@@ -138,15 +199,19 @@ for t in range(Nsim):
     status = solver.stats()["return_status"]
     optvar = var(sol["x"])
     
+    parguess["Ad",:], parguess["Bd",:], parguess["fd",:] = zip(*map(_calc_lin_disc_wrapper_for_mp_map,
+             zip([ode_casadi for _k in xrange(Nt)],
+                  optvar["x"],
+                  optvar["u"],
+                  [Delta for _k in xrange(Nt)] )))    
     #<<ENDCHUNK>>    
-    
     t1 = time.time()
     # Print stats.
     print "%d: %s in %.4f seconds" % (t,status, t1 - t0)
     u[t,:] = optvar["u",0,:]
     
-    #<<ENDCHUNK>>    
-    
+    #<<ENDCHUNK>>
+
     # Simulate.
     vdpargs = dict(x0=x[t,:],
                    p=u[t,:])
